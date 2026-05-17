c74c25504cec946188ecf64a710ee0d5d07270f7
Hameed Ibrahim <hameedibrh@gmail.com>
2026-05-17 15:49:57 +0300

    update socketio

--- backend/app.py
+++ backend/app.py
@@ -226,4 +226,4 @@ if __name__ == '__main__':
     logger.info("  stop_metrics_stream")
     logger.info("  request_metrics")
     
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False, allow_unsafe_werkzeug=True)
